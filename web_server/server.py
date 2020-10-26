95fe5b36fa6875d495f19a3d6853765f39638b79
Jonas Peter Hyatt <jph@dbc.dk>
2020-10-26 12:10:25 +0100

    add events to get data to the frontend, and add a couple of js libraries

--- web_server/server.py
+++ web_server/server.py
@@ -1,28 +1,93 @@
 import uvicorn
 from aiohttp import web
+import asyncio
 import socketio
+import time
+import numpy as np
 
 sio = socketio.AsyncServer()
 app = web.Application()
 sio.attach(app)
+sessions = {}
+
 
 async def index(request):
     """Serve the client-side application."""
     with open('index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
 
+
+##### BROWSER EVENTS #####
+# Get the temperatures
 @sio.event
-def connect(sid, environ):
-    print("connect ", sid)
+async def b_get_temperatures(sid):
+    # Send (mocked) temperatures
+    temperatures = {
+        't_still': round(float(np.abs(np.random.normal(2.2212))), 4),
+        't_1': round(float(np.abs(np.random.normal(3.3313))), 4),
+        't_2': round(float(np.abs(np.random.normal(4.4414))), 4),
+        't_3': round(float(np.abs(np.random.normal(5.5515))), 4),
+        't_4': round(float(np.abs(np.random.normal(6.6616))), 4),
+        't_5': round(float(np.abs(np.random.normal(7.7717))), 4),
+        't_6': round(float(np.abs(np.random.normal(8.8818))), 4),
+        'timestamp': time.time()
+    }
+
+    await sio.emit('b_temperatures', temperatures, room=sid)
+
+
+# Get the field strength of the large magnet
+@sio.event
+async def b_get_dc_field(sid):
+    dc_field_strength = round(float(np.abs(np.random.normal(8, 0.2))), 4)
+    await sio.emit('b_dc_field', dc_field_strength, room=sid)
+
+
+# Get the field strength of the small magnet
+@sio.event
+async def b_get_ac_field(sid):
+    ac_field_strength = round(float(np.random.normal(loc=0.0, scale=0.5)), 4)
+    await sio.emit('b_ac_field', ac_field_strength, room=sid)
+
+
+# Number of datapoints collected
+@sio.event
+async def b_get_n_points_taken(sid):
+    n_points_taken = 21
+    await sio.emit('b_n_points_taken', n_points_taken, room=sid)
+
+
+# Total number of datapoints to be collected during this run
+@sio.event
+async def b_get_n_points_total(sid):
+    n_points_total = 210
+    await sio.emit('b_n_points_total', n_points_total, room=sid)
+
 
 @sio.event
-async def chat_message(sid, data):
-    print("message ", data)
-    await sio.emit('reply', room=sid)
+async def b_get_rms(sid):
+    rms_value = round(float(np.abs(np.random.normal(0.545535))), 5)
+    await sio.emit('b_rms', rms_value, room=sid)
+
+
+##### UNIVERSAL EVENTS #####
+@sio.event
+async def connect(sid, environ):
+    print("connect ", sid)
+    await asyncio.sleep(0.5)
+    await sio.emit('idn', room=sid)
+
+
+@sio.event
+async def idn(sid, data):
+    print("idn ", data)
+    sessions[sid] = data
+
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
+
 
 app.router.add_static('/static', 'static')
 app.router.add_get('/', index)
--- web_server/static/socket_connection.js
+++ web_server/static/socket_connection.js
@@ -1,3 +1,19 @@
+// Create a state tree to contain page state
+const state = {
+    'last_update': 1603707641.0,
+    't_still': 0.0,
+    't_1': 0.0,
+    't_2': 0.0,
+    't_3': 0.0,
+    't_4': 0.0,
+    't_5': 0.0,
+    't_6': 0.0,
+    'ac_field': 0.0,
+    'dc_field': 0.0,
+    'n_points_taken': 0,
+    'n_points_total': 0
+};
+
 function main_socket_connection() {
     // Open a socket connection
     var socket = io();
@@ -5,34 +21,6 @@ function main_socket_connection() {
     // Set it to the window, so we can access it easily during debugging
     window.my_socket = socket;
 
-    // Setup events
-
-    // Open status page by default
-    window.pagehandlers['status_page']();
-}
-
-function open_status_page() {
-    $('.content-title').text('Experiment status');
-    $('.content-container').html(`
-    View the progress here
-    `);
-}
-
-function open_experiment_config_page() {
-    $('.content-title').text('Experiment configuration');
-    $('.content-container').html(`
-    Configure the experiment here!
-    `);
-}
-
-function open_info_page() {
-    $('.content-title').text('About this page');
-    $('.content-container').html(`
-    This page was made for a thesis
-    `);
-}
-
-$(function() {
     // Set pagehandlers to window
     window.pagehandlers = {
         status_page: open_status_page,
@@ -40,6 +28,176 @@ $(function() {
         info_page: open_info_page
     };
 
+    // Setup events
+    // Get events
+    socket.on('idn', idn_requested);
+    socket.on('b_temperatures', temperatures_updated);
+    socket.on('b_dc_field', dc_field_updated);
+    socket.on('b_ac_field', ac_field_updated);
+    socket.on('b_n_points_taken', n_points_taken_updated);
+    socket.on('b_n_points_total', n_points_total_updated);
+    socket.on('b_rms', rms_updated);
+
+    // Open status page by default
+    open_status_page();
+}
+
+// Event handler for server requesting idn
+function idn_requested() {
+    // Grab the socket connection
+    const socket = window.my_socket;
+
+    // Grab the idn from users cookies
+    let idn = Cookies.get('idn');
+
+    // Check if it exists already, if not we create a new one
+    if (typeof (idn) === 'undefined') {
+        idn = `webbrowser_${socket.id}`;
+        Cookies.set('idn', idn, {'SameSite': 'Strict'});
+    }
+
+    // Send the idn back to the server
+    socket.emit('idn', idn);
+}
+
+function update_state_tree() {
+    update_temperatures();
+    update_ac_field();
+    update_dc_field();
+    update_n_points_taken();
+    update_n_points_total();
+}
+
+function update_temperatures() {
+    // Request an update for the temperatures
+    window.my_socket.emit('b_get_temperatures');
+}
+
+function update_dc_field() {
+    // Request an update for the dc field
+    window.my_socket.emit('b_get_dc_field');
+}
+
+function update_ac_field() {
+    // Request an update for the ac field
+    window.my_socket.emit('b_get_ac_field');
+}
+
+function update_n_points_taken() {
+    // Request an update for the number of points taken
+    window.my_socket.emit('b_get_n_points_taken');
+}
+
+function update_n_points_total() {
+    // Request an update for the number of points total
+    window.my_socket.emit('b_get_n_points_total');
+}
+
+function update_rms() {
+    // Request an update for the rms
+    window.my_socket.emit('b_get_rms');
+}
+
+function temperatures_updated(temperatures) {
+    state['t_still'] = temperatures['t_still'];
+    state['t_1'] = temperatures['t_1'];
+    state['t_2'] = temperatures['t_2'];
+    state['t_3'] = temperatures['t_3'];
+    state['t_4'] = temperatures['t_4'];
+    state['t_5'] = temperatures['t_5'];
+    state['t_6'] = temperatures['t_6'];
+    state['last_update'] = temperatures['timestamp'];
+    state['current_page'](false);
+}
+
+function dc_field_updated(fieldstrength) {
+    state['dc_field'] = fieldstrength;
+    state['current_page'](false);
+}
+
+function ac_field_updated(fieldstrength) {
+    state['ac_field'] = fieldstrength;
+    state['current_page'](false);
+}
+
+function n_points_taken_updated(n_points_taken) {
+    state['n_points_taken'] = n_points_taken;
+    state['current_page'](false);
+}
+
+function n_points_total_updated(n_points_total) {
+    state['n_points_total'] = n_points_total;
+    state['current_page'](false);
+}
+
+function rms_updated(b_rms) {
+    state['b_rms'] = b_rms;
+    state['current_page'](false);
+}
+
+// Function to setup the DOM to contain the statuspage
+function open_status_page(should_update) {
+    state['current_page'] = open_status_page;
+    if (typeof(should_update) === 'undefined') {
+        update_state_tree();
+    }
+
+    $('.content-title').text('Experiment status').attr('id', 'status');
+    $('.content-container').html(`
+<div class="cryogenics_status status_container">
+    <h1 class="h4">Cryogenics status</h1>
+    <div>t_still = ${state['t_still']}K</div>
+    <div>t_1 = ${state['t_1']}K</div>
+    <div>t_2 = ${state['t_2']}K</div>
+    <div>t_3 = ${state['t_3']}K</div>
+    <div>t_4 = ${state['t_4']}K</div>
+    <div>t_5 = ${state['t_5']}K</div>
+    <div>t_6 = ${state['t_6']}K</div>
+</div>
+
+<div class="magnets_status status_container">
+    <h1 class="h4">Magnet status</h1>
+    <div>B_large = ${state['dc_field']}T</div>
+    <div>B_small = ${state['ac_field']}T</div>
+</div>
+
+<div class="datapoints_status status_container">
+    <h1 class="h4">Data status</h1>
+    <div>Number of datapoints taken = ${state['n_points_taken']}</div>
+    <div>Number of datapoints remaining = ${state['n_points_total'] - state['n_points_taken']}</div>
+    <div>Number of datapoints total = ${state['n_points_total']}</div>
+</div>
+    `);
+}
+
+// Function to setup the DOM to contain the experiment config page
+function open_experiment_config_page(should_update) {
+    state['current_page'] = open_experiment_config_page;
+    if (typeof(should_update) === 'undefined') {
+        update_state_tree();
+    }
+
+    $('.content-title').text('Experiment configuration').attr('id', 'config');
+    $('.content-container').html(`
+    Configure the experiment here!
+    `);
+}
+
+// Function to setup the DOM to contain the info page
+function open_info_page(should_update) {
+    state['current_page'] = open_info_page;
+    if (typeof(should_update) === 'undefined') {
+        update_state_tree();
+    }
+
+    $('.content-title').text('About this page').attr('id', 'about');
+    $('.content-container').html(`
+    This page was made for a thesis
+    `);
+}
+
+// Entrypoint, is called on document ready event
+$(function () {
     // Establish connection
     main_socket_connection();
 });
