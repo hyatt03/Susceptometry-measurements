8ff9dd83bc9a6796efe016dea26feeac9429e2b7
Jonas Peter Hyatt <jonas@hyatt.space>
2021-02-22 11:37:27 +0100

    add batch scripts to start the server and magnetism client

---
+++ start_magnetism_client.bat
@@ -0,0 +1,1 @@
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\ProgramData\Anaconda3' ; cd C:\Susceptometry\susceptometry_measurements\socket_clients ; python .\magnetism_client.py"
